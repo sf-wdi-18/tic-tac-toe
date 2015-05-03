246117918193850175fbce1c6ad0e62cefc0d0e7
rycwilson <rycwilson@gmail.com>
2015-05-03 00:32:24 -0700

    game logic and reset are functioning

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var squares;
 var squareBorders;
 var reset;
 var turn = 'X';
+var playCount = 0;
 
-
-window.addEventListener('load', function (){
+window.addEventListener('load', function () {
 
   // Grab all the necessary elements
   board = document.querySelector('body');
@@ -16,15 +16,15 @@ window.addEventListener('load', function (){
   squareBorders = document.querySelectorAll('.square');
   reset = document.querySelector('.reset');
 
-
   // Listen for clicks on squares
   // Track variable 'turn' for next move and end of game
   // Disallow repeated moves on the same square
   // Disallow moves after the game has been won
   for (var i = 0; i < squares.length; i++) {
     squares[i].addEventListener('click', function(event) {
-      if (squareEmpty(event.target)) {
+      if (event.target.innerHTML === '' && turn !== 'game_over') {  // square hasn't been played
         setSquare(event.target);
+        checkGameStatus();
       }
       else if (turn === 'game_over') {
         alert("The game is over!  Click 'reset' to play again.");
@@ -35,181 +35,114 @@ window.addEventListener('load', function (){
     });
   }
 
+  // Reset the board
+  reset.addEventListener('click', function () {
+    for (var i = 0; i < squares.length; i++) {
+      squares[i].innerHTML = '';
+      squares[i].setAttribute('class', 'blank');
+      // reset any highlighted winning squares
+      squareBorders[i].setAttribute('class', 'square');
+      turn = 'X';
+      playCount = 0;
+    }
+  });
+
 });
 
+// square is a p element
 function setSquare(square) {
   square.innerHTML = turn;  // turn = X or O
   square.setAttribute('class', turn);
   turn === 'X' ? turn = 'O' : turn = 'X';
 }
 
-function squareEmpty(square) {
-  return square.innerHTML === '';
+function checkGameStatus() {
+  var winner;
+  if (winner = getWinner()) {
+    console.log(winner);
+    gameOver(winner);
+  }
+  else if (++playCount === 9)
+    gameOver('tie');
+}
+
+function gameOver(winner) {
+  if (winner === 'tie') {
+    alert("Tie!");
+  }
+  else {
+    highlightBorders(winner);
+    // winner[0] is 'X' or 'O'
+    alert(winner[0] + ' won this round!');
+  }
+  turn = 'game_over';
+  // increment player wins
+  // check for hat trick
+}
+
+// getWinner will return a win in the form of a string denoting
+// the winning player and winning squares, e.g. 'X048'
+// if no win, return null
+function getWinner() {
+  if (winnerIs('X')) {
+    return 'X' + winnerIs('X');
+  }
+  else if (winnerIs('O')) {
+    console.log(winnerIs('O'));
+    return 'O' + winnerIs('O');
+  }
+  else
+    return null;
+}
+
+// If a winning combination of squares exists, return the win
+// in the form of a string denoting the winning squares, e.g. '048'
+function winnerIs(player) {
+  return allThree(player, squareValue('0'), squareValue('1'), squareValue('2')) ||
+         allThree(player, squareValue('3'), squareValue('4'), squareValue('5')) ||
+         allThree(player, squareValue('6'), squareValue('7'), squareValue('8')) ||
+         allThree(player, squareValue('0'), squareValue('3'), squareValue('6')) ||
+         allThree(player, squareValue('1'), squareValue('4'), squareValue('7')) ||
+         allThree(player, squareValue('2'), squareValue('5'), squareValue('8')) ||
+         allThree(player, squareValue('0'), squareValue('4'), squareValue('8')) ||
+         allThree(player, squareValue('2'), squareValue('4'), squareValue('6'));
+}
+
+function allThree(player, squareOne, squareTwo, squareThree) {
+  if ((squareOne[0] === player) && (squareTwo[0] === player) && (squareThree[0] === player))
+    return squareOne[1] + squareTwo[1] + squareThree[1];
+}
+
+function squareValue(key) {
+  switch(key) {
+    // we want to persist the data denoting the location of the square on the board,
+    // as this will allow us to highlight winning squares in the event of a win
+    // example return values are 'X5', '02', '7' (space hasn't been played)
+    case '0': return squares[0].innerHTML + '0';
+    case '1': return squares[1].innerHTML + '1';
+    case '2': return squares[2].innerHTML + '2';
+    case '3': return squares[3].innerHTML + '3';
+    case '4': return squares[4].innerHTML + '4';
+    case '5': return squares[5].innerHTML + '5';
+    case '6': return squares[6].innerHTML + '6';
+    case '7': return squares[7].innerHTML + '7';
+    case '8': return squares[8].innerHTML + '8';
+    default : console.log('Something went wrong');
+  }
+}
+
+// winningSquares is in the form of 'X048'
+function highlightBorders(winningSquares) {
+  console.log(winningSquares);
+  console.log(squareBorders);
+  squareBorders[parseInt(winningSquares[1])].setAttribute('class', 'win');
+  squareBorders[parseInt(winningSquares[2])].setAttribute('class', 'win');
+  squareBorders[parseInt(winningSquares[3])].setAttribute('class', 'win');
 }
 
 
-// original work below
 
 
 
 
-// // If a winning combo is found, the combo is passed to win().
-// // Color borders of the winning squares.
-// function win(winning_squares) {
-//   switch (winning_squares) {
-//     case "012":
-//       squareBorders[0].style.borderColor = "yellow";
-//       squareBorders[1].style.borderColor = "yellow";
-//       squareBorders[2].style.borderColor = "yellow";
-//       break;
-//     case "345":
-//       squareBorders[3].style.borderColor = "yellow";
-//       squareBorders[4].style.borderColor = "yellow";
-//       squareBorders[5].style.borderColor = "yellow";
-//       break;
-//     case "678":
-//       squareBorders[6].style.borderColor = "yellow";
-//       squareBorders[7].style.borderColor = "yellow";
-//       squareBorders[8].style.borderColor = "yellow";
-//       break;
-//     case "036":
-//       squareBorders[0].style.borderColor = "yellow";
-//       squareBorders[3].style.borderColor = "yellow";
-//       squareBorders[6].style.borderColor = "yellow";
-//       break;
-//     case "147":
-//       squareBorders[1].style.borderColor = "yellow";
-//       squareBorders[4].style.borderColor = "yellow";
-//       squareBorders[7].style.borderColor = "yellow";
-//       break;
-//     case "258":
-//       squareBorders[2].style.borderColor = "yellow";
-//       squareBorders[5].style.borderColor = "yellow";
-//       squareBorders[8].style.borderColor = "yellow";
-//       break;
-//     case "048":
-//       squareBorders[0].style.borderColor = "yellow";
-//       squareBorders[4].style.borderColor = "yellow";
-//       squareBorders[8].style.borderColor = "yellow";
-//       break;
-//     case "246":
-//       squareBorders[2].style.borderColor = "yellow";
-//       squareBorders[4].style.borderColor = "yellow";
-//       squareBorders[6].style.borderColor = "yellow";
-//       break;
-//     default:
-//       break;
-//   }
-// }
-
-// // Reset button - clear squares and reset CSS class
-// reset.addEventListener("click", function() {
-//   for (var i = 0; i < squares.length; i++) {
-//     squares[i].innerHTML = "";
-//     squares[i].setAttribute("class", "markNone");
-//     squareBorders[i].style.borderColor = "black";
-//     turn = "X";
-//   }
-// });
-
-// // Listen for a click anywhere on the board.
-// // Check for a winning combo of squares.
-// board.addEventListener("click", function() {
-//   if (squares[0].innerHTML == "X"
-//       && squares[1].innerHTML == "X"
-//       && squares[2].innerHTML == "X") {
-//     win("012");
-//     turn = "over";
-//   }
-//   else if (squares[3].innerHTML == "X"
-//       && squares[4].innerHTML == "X"
-//       && squares[5].innerHTML == "X") {
-//     win("345");
-//     turn = "over";
-//   }
-//   else if (squares[6].innerHTML == "X"
-//       && squares[7].innerHTML == "X"
-//       && squares[8].innerHTML == "X") {
-//     win("678");
-//     turn = "over";
-//   }
-//   else if (squares[0].innerHTML == "X"
-//       && squares[3].innerHTML == "X"
-//       && squares[6].innerHTML == "X") {
-//     win("036");
-//     turn = "over";
-//   }
-//   else if (squares[1].innerHTML == "X"
-//       && squares[4].innerHTML == "X"
-//       && squares[7].innerHTML == "X") {
-//     win("147");
-//     turn = "over";
-//   }
-//   else if (squares[2].innerHTML == "X"
-//       && squares[5].innerHTML == "X"
-//       && squares[8].innerHTML == "X") {
-//     win("258");
-//     turn = "over";
-//   }
-//   else if (squares[0].innerHTML == "X"
-//       && squares[4].innerHTML == "X"
-//       && squares[8].innerHTML == "X") {
-//     win("048");
-//     turn = "over";
-//   }
-//   else if (squares[2].innerHTML == "X"
-//       && squares[4].innerHTML == "X"
-//       && squares[6].innerHTML == "X") {
-//     win("246");
-//     turn = "over";
-//   }
-//   else if (squares[0].innerHTML == "O"
-//       && squares[1].innerHTML == "O"
-//       && squares[2].innerHTML == "O") {
-//     win("012");
-//     turn = "over";
-//   }
-//   else if (squares[3].innerHTML == "O"
-//       && squares[4].innerHTML == "O"
-//       && squares[5].innerHTML == "O") {
-//     win("345");
-//     turn = "over";
-//   }
-//   else if (squares[6].innerHTML == "O"
-//       && squares[7].innerHTML == "O"
-//       && squares[8].innerHTML == "O") {
-//     win("678");
-//     turn = "over";
-//   }
-//   else if (squares[0].innerHTML == "O"
-//       && squares[3].innerHTML == "O"
-//       && squares[6].innerHTML == "O") {
-//     win("036");
-//     turn = "over";
-//   }
-//   else if (squares[1].innerHTML == "O"
-//       && squares[4].innerHTML == "O"
-//       && squares[7].innerHTML == "O") {
-//     win("147");
-//     turn = "over";
-//   }
-//   else if (squares[2].innerHTML == "O"
-//       && squares[5].innerHTML == "O"
-//       && squares[8].innerHTML == "O") {
-//     win("258");
-//     turn = "over";
-//   }
-//   else if (squares[0].innerHTML == "O"
-//       && squares[4].innerHTML == "O"
-//       && squares[8].innerHTML == "O") {
-//     win("048");
-//     turn = "over";
-//   }
-//   else if (squares[2].innerHTML == "O"
-//       && squares[4].innerHTML == "O"
-//       && squares[6].innerHTML == "O") {
-//     win("246");
-//     turn = "over";
-//   }
-// });
 
